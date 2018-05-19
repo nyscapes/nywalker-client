3d69491f9ec15cb46bff57a7e5a78f3934f3f492
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-05-19 19:04:16 -0400

    install bootstrap

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,11 @@ const EmberApp = require("ember-cli/lib/broccoli/ember-app");
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
+    "ember-bootstrap": {
+      "bootstrapVersion": 4,
+      "importBootstrapFont": true,
+      "importBootstrapCSS": true
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
@@ -12,7 +16,7 @@ module.exports = function(defaults) {
   //
   // If you need to use different assets in different
   // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
+  // object"s keys should be the environment name and the values
   // should be the asset to use in that environment.
   //
   // If the library that you are including contains AMD or ES6
