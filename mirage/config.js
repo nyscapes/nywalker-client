31da1b36d25fc68c50d0ae119c1c06fac34499e8
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-05-20 00:37:58 -0400

    Add mirage to linting.

--- mirage/config.js
+++ mirage/config.js
@@ -5,7 +5,7 @@ export default function() {
     Note: these only affect routes defined *after* them!
   */
 
-  this.urlPrefix = "http://localhost:9292"
+  this.urlPrefix = "http://localhost:9292";
   // this.urlPrefix = "";    // make this `http://localhost:8080`, for example, if your API is on a different server
   this.namespace = "/api/v1";    // make this `/api`, for example, if your API is namespaced
   // this.timing = 400;      // delay for each request, automatically set to 0 during testing
@@ -35,45 +35,45 @@ export default function() {
       },
       {
         "type": "books",
-          "id": "2",
-          "attributes": {
-            "slug": "the-age-of-innocence-1920",
-            "title": "The Age of Innocence",
-            "author": "Edith Wharton",
-            "isbn": "9780375753206",
-            "year": 1920,
-            "url": "http://books.google.com/books?id=vGq7MofJ4scC&dq=isbn:9780375753206&hl=&source=gbs_api",
-            "cover": "http://books.google.com/books/content?id=vGq7MofJ4scC&printsec=frontcover&img=1&zoom=1&source=gbs_api",
-            "added-on": "2016-01-22",
-            "modified-on": null,
-            "total-pages": 265,
-            "instances-per-page": 2.5,
-            "instance-count": 663
-          },
-          "links": {
-              "self": "/api/v1/books/2"
-          }
+        "id": "2",
+        "attributes": {
+          "slug": "the-age-of-innocence-1920",
+          "title": "The Age of Innocence",
+          "author": "Edith Wharton",
+          "isbn": "9780375753206",
+          "year": 1920,
+          "url": "http://books.google.com/books?id=vGq7MofJ4scC&dq=isbn:9780375753206&hl=&source=gbs_api",
+          "cover": "http://books.google.com/books/content?id=vGq7MofJ4scC&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+          "added-on": "2016-01-22",
+          "modified-on": null,
+          "total-pages": 265,
+          "instances-per-page": 2.5,
+          "instance-count": 663
+        },
+        "links": {
+          "self": "/api/v1/books/2"
+        }
       },
       {
         "type": "books",
-          "id": "10",
-          "attributes": {
-            "slug": "the-crying-of-lot-49-1986-ed-dot-1965",
-            "title": "The Crying of Lot 49 (1986 ed.)",
-            "author": "Thomas Pynchon",
-            "isbn": "9780060913076",
-            "year": 1965,
-            "url": "http://books.google.com/books?id=51oT_Xk3TIUC&dq=isbn:9780060913076&hl=&source=gbs_api",
-            "cover": "http://books.google.com/books/content?id=51oT_Xk3TIUC&printsec=frontcover&img=1&zoom=1&source=gbs_api",
-            "added-on": "2016-11-27",
-            "modified-on": null,
-            "total-pages": 173,
-            "instances-per-page": 2.55,
-            "instance-count": 442
-          },
-          "links": {
-            "self": "/api/v1/books/10"
-          }
+        "id": "10",
+        "attributes": {
+          "slug": "the-crying-of-lot-49-1986-ed-dot-1965",
+          "title": "The Crying of Lot 49 (1986 ed.)",
+          "author": "Thomas Pynchon",
+          "isbn": "9780060913076",
+          "year": 1965,
+          "url": "http://books.google.com/books?id=51oT_Xk3TIUC&dq=isbn:9780060913076&hl=&source=gbs_api",
+          "cover": "http://books.google.com/books/content?id=51oT_Xk3TIUC&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+          "added-on": "2016-11-27",
+          "modified-on": null,
+          "total-pages": 173,
+          "instances-per-page": 2.55,
+          "instance-count": 442
+        },
+        "links": {
+          "self": "/api/v1/books/10"
+        }
       }],
       "meta": {
         "copyright": "Copyright 2018 Moacir P. de Sá Pereira",
@@ -82,7 +82,7 @@ export default function() {
           "Moacir P. de Sá Pereira"
         ]
       }
-    }
+    };
   });
 
 
@@ -91,7 +91,7 @@ export default function() {
 
 }
     
-  /*
+/*
     Shorthand cheatsheet:
 
     this.get("/posts");
