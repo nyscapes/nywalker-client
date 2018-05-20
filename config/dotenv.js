862c7f5f8aaf5588252fdce0e3676d13b375d6d6
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-05-20 00:22:44 -0400

    Fix some linting.

--- config/dotenv.js
+++ config/dotenv.js
@@ -1,4 +1,4 @@
-module.exports = function(env) {
+module.exports = function() {
   return {
     clientAllowedKeys: ["AUTH_CONFIG_CLIENT_ID", "AUTH_CONFIG_DOMAIN", "AUTH_CONFIG_CALLBACK_URL", "AUTH_CONFIG_API_URL"],
     failOnMissingKey: false,
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -27,7 +27,7 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
  
-  app.import('vendor/auth0.js');
+  app.import("vendor/auth0.js");
 
   return app.toTree();
 };
