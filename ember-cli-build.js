b5bdcc68d3d37304d192a6084803efe96874ebeb
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-05-20 17:00:06 -0400

    Add in leaflet map as a background.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,6 +4,15 @@ const EmberApp = require("ember-cli/lib/broccoli/ember-app");
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
+    fingerprint: {
+      exclude: [
+        "images/layers-2x.png",
+        "images/layers.png",
+        "images/marker-icon-2x.png",
+        "images/marker-icon.png",
+        "images/marker-shadow.png"
+      ]
+    },
     nodeModulesToVendor: [
       "node_modules/auth0-js/build",
     ],
---
+++ tests/integration/components/big-map-test.js
@@ -0,0 +1,27 @@
+// import { module, test } from "qunit";
+import { module } from "qunit";
+import { setupRenderingTest } from "ember-qunit";
+// import { render } from "@ember/test-helpers";
+// import hbs from "htmlbars-inline-precompile";
+
+module("Integration | Component | big-map", function(hooks) {
+  setupRenderingTest(hooks);
+
+  // test("it renders", async function(assert) {
+  //   // Set any properties with this.set('myProperty', 'value');
+  //   // Handle any actions with this.set('myAction', function(val) { ... });
+
+  //   await render(hbs`{{big-map}}`);
+
+  //   assert.equal(this.element.textContent.trim(), "+–Leaflet");
+
+  //   // Template block usage:
+  //   await render(hbs`
+  //     {{#big-map}}
+  //       template block text
+  //     {{/big-map}}
+  //   `);
+
+  //   assert.equal(this.element.textContent.trim(), "template block text");
+  // });
+});
