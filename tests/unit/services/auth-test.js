5ea9f5eb393776da22d757f4f237634eaf3f7306
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-05-19 23:18:06 -0400

    Blank out test for auth0

--- tests/unit/services/auth-test.js
+++ tests/unit/services/auth-test.js
@@ -1,13 +1,24 @@
 import { module, test } from "qunit";
+// import { module } from "qunit";
 import { setupTest } from "ember-qunit";
 
 module("Unit | Service | auth", function(hooks) {
   setupTest(hooks);
 
-  // Replace this with your real tests.
+  //Replace this with your real tests.
   test("it exists", function(assert) {
     let service = this.owner.lookup("service:auth");
     assert.ok(service);
   });
+
+  // test("should return an auth0.WebAuth object with a scope of 'openid'", function(assert) {
+  //   const auth = this.owner.lookup("service:auth");
+  //   // auth.set("auth0");
+  //   assert.equal(auth.get("auth0").scope, "openid");
+  // });
+
+
+
+
 });
 
