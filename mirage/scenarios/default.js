d56358368ffb11fe1827dcf57d56403df3ca0b32
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-05-20 17:46:00 -0400

    Wire up mirage more appropriately.

--- mirage/scenarios/default.js
+++ mirage/scenarios/default.js
@@ -1,9 +1,8 @@
-export default function(/* server */) {
+export default function( server ) {
 
-  /*
-    Seed your development database using your factories.
-    This data will not be loaded in your tests.
-  */
+  server.loadFixtures();
 
   // server.createList('post', 10);
 }
+
+
