496419e4b2d0f4d545b5d75b963f58f8f503856a
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-05-25 01:30:23 -0400

    Tighten mirage config w/ shorthand.

--- mirage/config.js
+++ mirage/config.js
@@ -9,32 +9,11 @@ export default function() {
   this.namespace = "/api/v1";
   this.timing = 400;
 
-  this.get("/books", (schema) => {
-    return schema.books.all();
-  });
-
-  this.get("/books/:id");
-
-  this.get("/instances", (schema) => {
-    return schema.instances.all();
-  });
-
-  this.get("/places", (schema) => {
-    return schema.places.all();
-  });
-
-  this.get("/nicknames", (schema) => {
-    return schema.nicknames.all();
-  });
+  this.resource("books"); // Shorthand
+  this.resource("instances");
+  this.resource("places");
+  this.resource("nicknames");
 
   this.passthrough("http://api.geonames.org/**");
 
 }
-
-/*
-  Shorthand cheatsheet:
-  this.post("/posts");
-  this.patch("/posts/:id");
-  this.del("/posts/:id");
-  http://www.ember-cli-mirage.com/docs/v0.3.x/shorthands/
-*/
