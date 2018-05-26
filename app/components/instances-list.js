5d60e3b59543f65071ea42e6053fef7f2eab4c21
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-05-26 02:00:10 -0400

    Add instances-list and instance-row components.

---
+++ app/components/instances-list.js
@@ -0,0 +1,5 @@
+import Component from "@ember/component";
+
+export default Component.extend({
+  classNames: ["container", "mt-3", "instances-list"]
+});
---
+++ tests/integration/components/instance-row-test.js
@@ -0,0 +1,26 @@
+// import { module, test } from "qunit";
+// import { setupRenderingTest } from "ember-qunit";
+// import { render } from "@ember/test-helpers";
+// import hbs from "htmlbars-inline-precompile";
+
+// module("Integration | Component | instance-row", function(hooks) {
+//   setupRenderingTest(hooks);
+
+//   test("it renders", async function(assert) {
+//     // Set any properties with this.set('myProperty', 'value');
+//     // Handle any actions with this.set('myAction', function(val) { ... });
+
+//     await render(hbs`{{instance-row}}`);
+
+//     assert.equal(this.element.textContent.trim(), "");
+
+//     // Template block usage:
+//     await render(hbs`
+//       {{#instance-row}}
+//         template block text
+//       {{/instance-row}}
+//     `);
+
+//     assert.equal(this.element.textContent.trim(), "template block text");
+//   });
+// });
