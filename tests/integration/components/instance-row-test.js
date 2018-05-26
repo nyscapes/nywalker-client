10002f7fb9ad84037d492d42a69801482db2116a
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-05-26 12:17:46 -0400

    Prepare instance-row for testing by giving useful ids to the divs.

--- tests/integration/components/instance-row-test.js
+++ tests/integration/components/instance-row-test.js
@@ -1,10 +1,26 @@
-// import { module, test } from "qunit";
-// import { setupRenderingTest } from "ember-qunit";
-// import { render } from "@ember/test-helpers";
-// import hbs from "htmlbars-inline-precompile";
+import { module, test } from "qunit";
+import { setupRenderingTest } from "ember-qunit";
+import { render } from "@ember/test-helpers";
+import hbs from "htmlbars-inline-precompile";
 
-// module("Integration | Component | instance-row", function(hooks) {
-//   setupRenderingTest(hooks);
+module("Integration | Component | instance-row", function(hooks) {
+  setupRenderingTest(hooks);
+
+  test("should present a row of columns", async function(assert) {
+    // assert.expect(3);
+    this.set("instance", {
+      page: 10,
+      sequence: 2,
+      text: "The City",
+      id: 42
+    });
+
+    await render(hbs`{{instance-row instance=instance}}`);
+    assert.equal(3, 3);
+  });
+
+
+
 
 //   test("it renders", async function(assert) {
 //     // Set any properties with this.set('myProperty', 'value');
@@ -23,4 +39,4 @@
 
 //     assert.equal(this.element.textContent.trim(), "template block text");
 //   });
-// });
+});
