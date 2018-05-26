c5a6eb47d2e3f82bc4bf3f0fb4b41db15771861a
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-05-26 01:15:50 -0400

    Add some properties to Book to help manage instances and places down the line.

--- app/models/book.js
+++ app/models/book.js
@@ -1,4 +1,6 @@
 import DS from "ember-data";
+import { computed } from "@ember/object";
+// import EmberArray from "@ember/array";
 
 export default DS.Model.extend({
   slug: DS.attr("string"),
@@ -13,7 +15,17 @@ export default DS.Model.extend({
   total_pages: DS.attr("number"),
   instance_count: DS.attr("number"),
   instances_per_page: DS.attr("number"),
+  special_field: DS.attr("string"),
+  special_help_text: DS.attr("string"),
+  last_instance: DS.attr("string"),
 
   instances: DS.hasMany("instance"),
 
+  mappable_places: computed("instances", function(){
+    return this.get("instances").map( instance => {
+      if(instance.mappable){
+        return { name: instance.place_name, lat: instance.lat, lon: instance.lon };
+      }
+    }).compact();
+  })
 });
