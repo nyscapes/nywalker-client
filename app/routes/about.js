25f72d4a3cebef88e55a27f7b59f13d88f1c18e9
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-05-27 03:36:01 -0400

    Add a card header for about

--- app/routes/about.js
+++ app/routes/about.js
@@ -1,4 +1,9 @@
 import Route from "@ember/routing/route";
+import { inject as service } from "@ember/service";
 
 export default Route.extend({
+  card: service(),
+  afterModel() {
+    this.set("card.header", "About NYWalker");
+  },
 });
