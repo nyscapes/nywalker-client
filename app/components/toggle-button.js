0bc347c562af375cd7c4761edacbf67f06a962ec
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-05-27 04:22:28 -0400

    let toggle-button change sizes

--- app/components/toggle-button.js
+++ app/components/toggle-button.js
@@ -2,7 +2,7 @@ import Component from "@ember/component";
 
 export default Component.extend({
   classNames: ["btn"],
-  classNameBindings: ["btnclass", "active"],
+  classNameBindings: ["btnsize", "btnclass", "active"],
   tagName: "button",
   attributeBindings: ["type", "data-target", "data-toggle", "aria-expanded", "aria-controls"],
   type: "button",
@@ -11,9 +11,16 @@ export default Component.extend({
   ariaExpanded: null,
   ariaControls: null,
   btnclass: "secondary",
+  btnsize: null,
+  buttonSize: null,
   active: false,
   didInsertElement() { 
+    const btnSize = this.get("buttonSize");
+    if(btnSize){
+      this.set("btnsize", `btn-${this.get("buttonSize")}`);
+    }
     this.set("btnclass", `btn-${this.get("buttonClass")}`);
+
   },
   click() { this.toggleProperty("active"); }
 });
